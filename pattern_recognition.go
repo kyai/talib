0834e652a78d22cd359bd9996bceca89e1ec18f5
kyai <hukuang@163.com>
2018-11-06 09:46:30 +0800

    commit

---
+++ pattern_recognition.go
@@ -0,0 +1,306 @@
+package talib
+
+// Two Crows
+func CDL2CROWS(klines []Kline) int {
+	return 0
+}
+
+// Three Black Crows
+func CDL3BLACKCROWS(klines []Kline) int {
+	return 0
+}
+
+// Three Inside Up/Down
+func CDL3INSIDE(klines []Kline) int {
+	return 0
+}
+
+// Three-Line Strike
+func CDL3LINESTRIKE(klines []Kline) int {
+	return 0
+}
+
+// Three Outside Up/Down
+func CDL3OUTSIDE(klines []Kline) int {
+	return 0
+}
+
+// Three Stars In The South
+func CDL3STARSINSOUTH(klines []Kline) int {
+	return 0
+}
+
+// Three Advancing White Soldiers
+func CDL3WHITESOLDIERS(klines []Kline) int {
+	return 0
+}
+
+// Abandoned Baby
+func CDLABANDONEDBABY(klines []Kline) int {
+	return 0
+}
+
+// Advance Block
+func CDLADVANCEBLOCK(klines []Kline) int {
+	return 0
+}
+
+// Belt-hold
+func CDLBELTHOLD(klines []Kline) int {
+	return 0
+}
+
+// Breakaway
+func CDLBREAKAWAY(klines []Kline) int {
+	return 0
+}
+
+// Closing Marubozu
+func CDLCLOSINGMARUBOZU(klines []Kline) int {
+	return 0
+}
+
+// Concealing Baby Swallow
+func CDLCONCEALBABYSWALL(klines []Kline) int {
+	return 0
+}
+
+// Counterattack
+func CDLCOUNTERATTACK(klines []Kline) int {
+	return 0
+}
+
+// Dark Cloud Cover
+func CDLDARKCLOUDCOVER(klines []Kline) int {
+	return 0
+}
+
+// Doji
+func CDLDOJI(klines []Kline) int {
+	return 0
+}
+
+// Doji Star
+func CDLDOJISTAR(klines []Kline) int {
+	return 0
+}
+
+// Dragonfly Doji
+func CDLDRAGONFLYDOJI(klines []Kline) int {
+	return 0
+}
+
+// Engulfing Pattern
+func CDLENGULFING(klines []Kline) int {
+	return 0
+}
+
+// Evening Doji Star
+func CDLEVENINGDOJISTAR(klines []Kline) int {
+	return 0
+}
+
+// Evening Star
+func CDLEVENINGSTAR(klines []Kline) int {
+	return 0
+}
+
+// Up/Down-gap side-by-side white lines
+func CDLGAPSIDESIDEWHITE(klines []Kline) int {
+	return 0
+}
+
+// Gravestone Doji
+func CDLGRAVESTONEDOJI(klines []Kline) int {
+	return 0
+}
+
+// Hammer
+func CDLHAMMER(klines []Kline) int {
+	return 0
+}
+
+// Hanging Man
+func CDLHANGINGMAN(klines []Kline) int {
+	return 0
+}
+
+// Harami Pattern
+func CDLHARAMI(klines []Kline) int {
+	return 0
+}
+
+// Harami Cross Pattern
+func CDLHARAMICROSS(klines []Kline) int {
+	return 0
+}
+
+// High-Wave Candle
+func CDLHIGHWAVE(klines []Kline) int {
+	return 0
+}
+
+// Hikkake Pattern
+func CDLHIKKAKE(klines []Kline) int {
+	return 0
+}
+
+// Modified Hikkake Pattern
+func CDLHIKKAKEMOD(klines []Kline) int {
+	return 0
+}
+
+// Homing Pigeon
+func CDLHOMINGPIGEON(klines []Kline) int {
+	return 0
+}
+
+// Identical Three Crows
+func CDLIDENTICAL3CROWS(klines []Kline) int {
+	return 0
+}
+
+// In-Neck Pattern
+func CDLINNECK(klines []Kline) int {
+	return 0
+}
+
+// Inverted Hammer
+func CDLINVERTEDHAMMER(klines []Kline) int {
+	return 0
+}
+
+// Kicking
+func CDLKICKING(klines []Kline) int {
+	return 0
+}
+
+// Kicking - bull/bear determined by the longer marubozu
+func CDLKICKINGBYLENGTH(klines []Kline) int {
+	return 0
+}
+
+// Ladder Bottom
+func CDLLADDERBOTTOM(klines []Kline) int {
+	return 0
+}
+
+// Long Legged Doji
+func CDLLONGLEGGEDDOJI(klines []Kline) int {
+	return 0
+}
+
+// Long Line Candle
+func CDLLONGLINE(klines []Kline) int {
+	return 0
+}
+
+// Marubozu
+func CDLMARUBOZU(klines []Kline) int {
+	return 0
+}
+
+// Matching Low
+func CDLMATCHINGLOW(klines []Kline) int {
+	return 0
+}
+
+// Mat Hold
+func CDLMATHOLD(klines []Kline) int {
+	return 0
+}
+
+// Morning Doji Star
+func CDLMORNINGDOJISTAR(klines []Kline) int {
+	return 0
+}
+
+// Morning Star
+func CDLMORNINGSTAR(klines []Kline) int {
+	return 0
+}
+
+// On-Neck Pattern
+func CDLONNECK(klines []Kline) int {
+	return 0
+}
+
+// Piercing Pattern
+func CDLPIERCING(klines []Kline) int {
+	return 0
+}
+
+// Rickshaw Man
+func CDLRICKSHAWMAN(klines []Kline) int {
+	return 0
+}
+
+// Rising/Falling Three Methods
+func CDLRISEFALL3METHODS(klines []Kline) int {
+	return 0
+}
+
+// Separating Lines
+func CDLSEPARATINGLINES(klines []Kline) int {
+	return 0
+}
+
+// Shooting Star
+func CDLSHOOTINGSTAR(klines []Kline) int {
+	return 0
+}
+
+// Short Line Candle
+func CDLSHORTLINE(klines []Kline) int {
+	return 0
+}
+
+// Spinning Top
+func CDLSPINNINGTOP(klines []Kline) int {
+	return 0
+}
+
+// Stalled Pattern
+func CDLSTALLEDPATTERN(klines []Kline) int {
+	return 0
+}
+
+// Stick Sandwich
+func CDLSTICKSANDWICH(klines []Kline) int {
+	return 0
+}
+
+// Takuri (Dragonfly Doji with very long lower shadow)
+func CDLTAKURI(klines []Kline) int {
+	return 0
+}
+
+// Tasuki Gap
+func CDLTASUKIGAP(klines []Kline) int {
+	return 0
+}
+
+// Thrusting Pattern
+func CDLTHRUSTING(klines []Kline) int {
+	return 0
+}
+
+// Tristar Pattern
+func CDLTRISTAR(klines []Kline) int {
+	return 0
+}
+
+// Unique 3 River
+func CDLUNIQUE3RIVER(klines []Kline) int {
+	return 0
+}
+
+// Upside Gap Two Crows
+func CDLUPSIDEGAP2CROWS(klines []Kline) int {
+	return 0
+}
+
+// Upside/Downside Gap Three Methods
+func CDLXSIDEGAP3METHODS(klines []Kline) int {
+	return 0
+}
---
+++ structs.go
@@ -0,0 +1,8 @@
+package talib
+
+type Kline struct {
+	Open  float64
+	High  float64
+	Low   float64
+	Close float64
+}
